e9c8f66b9f20dd60f0007e24fe77757a7c479cf9
Emilie Ickart <emi.ickart@gmail.com>
2023-04-29 11:52:54 +0200

    Add MenuItem.vue

---
+++ src/components/NavLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <a href="url">{{ text }}</a>
+</template>
+
+<script>
+export default {
+    name: "NavLink",
+    props: {
+        url: String,
+        text: String,
+    }
+}
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+</style>
